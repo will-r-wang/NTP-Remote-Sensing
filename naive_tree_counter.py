cbff30caaf622368f75e2f7bf3868b9b854954bb
William Wang <william.wang@shopify.com>
2021-07-15 19:51:23 -0400

    Add naive tree counter

---
+++ naive_tree_counter.py
@@ -0,0 +1,39 @@
+import argparse
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='naive tree count for an input image file')
+    parser.add_argument('-o', '--output', help='Output annotated image file name')
+    requiredNamed = parser.add_argument_group('required named arguments')
+    requiredNamed.add_argument('-i', '--input-image', help='Input image file name', required=True)
+    args = parser.parse_args()
+
+    image = cv2.imread(args.input_image)
+    height, width, _ = image.shape
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # RGB mask filtering
+    GREEN_MIN = np.array([74, 83, 52], np.uint8)
+    GREEN_MAX = np.array([150, 209, 193], np.uint8)
+
+    dst = cv2.inRange(hsv, GREEN_MIN, GREEN_MAX)
+
+    EST_PIXELS_PER_TREE = 1000
+    num_green_pixels = cv2.countNonZero(dst)
+    print('Total # of green pixels:', str(num_green_pixels))
+    print('Total estimated # of trees:', str(num_green_pixels//EST_PIXELS_PER_TREE))
+
+    mask = dst == 0
+    green = np.zeros_like(image, np.uint8)
+    green[mask] = image[mask]
+    cv2.imwrite("images/sample_trees_mask.tif", green)
+
+    # Image grid-line annotation
+    for i in range(1, height//400 + 1):
+        cv2.line(image, (0, i * 400), (width, i * 400), (0, 0, 255), 1)
+
+    for j in range(1, width//400 + 1):
+        cv2.line(image, (j * 400, 0), (j * 400, height), (0, 0, 255), 1)
+
+    cv2.imwrite("images/sample_trees_annotated.tif", image)
